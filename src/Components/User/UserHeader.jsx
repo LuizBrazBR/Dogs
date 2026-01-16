f00785efb27f1078eb3974559016b19bf71bbd9d
Luiz Braz <luiz.braz@evolua.software>
2026-01-16 16:37:22 -0300

    Menu mobile

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./UserHeader.module.css";
 import Feed from "../../Assets/feed.svg?react";
@@ -6,43 +6,64 @@ import Stats from "../../Assets/estatisticas.svg?react";
 import Post from "../../Assets/adicionar.svg?react";
 import Sair from "../../Assets/sair.svg?react";
 import { UserContext } from "../../UserContext";
+import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
 
 const UserHeader = () => {
-  const [mobile] = useState(false);
+  const mobile = useMediaQuery({ query: "(max-width: 50rem)" });
+  const [mobileActive, setMobileActive] = useState(false);
 
   const { logout } = useContext(UserContext);
 
+  function mobileMenu() {
+    setMobileActive(!mobileActive);
+  }
+
   return (
-    <nav className={styles.nav}>
-      <NavLink
-        to="/conta"
-        end
-        className={({ isActive }) => (isActive ? styles.active : "")}
+    <>
+      {mobile && (
+        <button
+          className={`${styles.mobileButton} ${mobileActive && styles.mobileButtonActive}`}
+          onClick={mobileMenu}
+        ></button>
+      )}
+
+      <nav
+        className={`${!mobile && styles.nav} ${mobile && styles.mobile} ${mobileActive && styles.mobileActive}`}
       >
-        <Feed />
-        {mobile && "Feed"}
-      </NavLink>
-      <NavLink
-        to="/conta/stats"
-        end
-        className={({ isActive }) => (isActive ? styles.active : "")}
-      >
-        <Stats />
-        {mobile && "Estatísticas"}
-      </NavLink>
-      <NavLink
-        to="/conta/postar"
-        end
-        className={({ isActive }) => (isActive ? styles.active : "")}
-      >
-        <Post />
-        {mobile && "Adicionar Foto"}
-      </NavLink>
-      <button onClick={logout}>
-        <Sair />
-        {mobile && "Sair"}
-      </button>
-    </nav>
+        <NavLink
+          to="/conta"
+          end
+          className={({ isActive }) => (isActive ? styles.active : "")}
+          onClick={() => setMobileActive(false)}
+        >
+          <Feed />
+          {mobile && "Feed"}
+        </NavLink>
+        <NavLink
+          to="/conta/stats"
+          end
+          className={({ isActive }) => (isActive ? styles.active : "")}
+          onClick={() => setMobileActive(false)}
+        >
+          <Stats />
+          {mobile && "Estatísticas"}
+        </NavLink>
+        <NavLink
+          to="/conta/postar"
+          end
+          className={({ isActive }) => (isActive ? styles.active : "")}
+          onClick={() => setMobileActive(false)}
+        >
+          <Post />
+          {mobile && "Adicionar Foto"}
+        </NavLink>
+        <button onClick={logout}>
+          <Sair />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
   );
 };
 
