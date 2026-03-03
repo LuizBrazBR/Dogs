a44a9ed86058d6c480f2a3a2890f862f5e8ed644
Luiz Braz <luiz.braz@evolua.software>
2026-03-03 11:51:17 -0300

    Loading animado

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import Spinner from "./Components/Spinner.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <App />
+  <Spinner />,
   // </StrictMode>
 );
