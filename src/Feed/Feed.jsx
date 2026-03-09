0676345b4fb7f275c300c3694b5adee76ddd6d93
Luiz Braz <luiz.braz@evolua.software>
2026-03-09 11:56:18 -0300

    Técnicas mais avançadas de infinite scroll

--- src/Feed/Feed.jsx
+++ src/Feed/Feed.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import FeedPhotos from './FeedPhotos';
-import FeedModal from './FeedModal';
+import React, { useEffect, useRef, useState } from "react";
+import FeedPhotos from "./FeedPhotos";
+import FeedModal from "./FeedModal";
 
 const TOTAL = 6;
 
@@ -8,6 +8,40 @@ const Feed = () => {
   const [modal, setModal] = useState(null);
 
   const [pages, setPage] = useState([1]);
+  const infinite = useRef(false); // ✅
+  const loadingApi = useRef(false); // guard síncrono
+  const [awaitApi, setAwaitApi] = useState(false);
+
+  useEffect(() => {
+    function carregarMais() {
+      if (awaitApi || infinite.current || loadingApi.current) return;
+
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 100
+      ) {
+        if (!awaitApi && !loadingApi.current) {
+          loadingApi.current = true;
+          setAwaitApi(true);
+          //prev[prev.length - 1] + 1 é mais robusto porque usa o último número real da lista
+          //para gerar a próxima página,
+          //enquanto prev.length + 1 depende apenas do tamanho do array e pode gerar número errado se faltar
+          //ou repetir páginas.
+          setPage((prev) => [...prev, prev[prev.length - 1] + 1]);
+        } else {
+          infinite.current = true;
+        }
+      }
+    }
+
+    window.addEventListener("scroll", carregarMais);
+    window.addEventListener("wheel", carregarMais);
+
+    return () => {
+      window.removeEventListener("scroll", carregarMais);
+      window.removeEventListener("wheel", carregarMais);
+    };
+  }, [awaitApi, infinite, setPage]);
 
   return (
     <>
@@ -21,6 +55,9 @@ const Feed = () => {
             page={page}
             total={TOTAL}
             setPage={setPage}
+            infinite={infinite}
+            loadingApi={loadingApi}
+            setAwaitApi={setAwaitApi}
           />
         );
       })}
--- src/Feed/FeedPhotos.jsx
+++ src/Feed/FeedPhotos.jsx
@@ -1,20 +1,26 @@
-import React, { useEffect, useRef, useState } from 'react';
-import useFetch from '../Hooks/useFetch';
-import { PHOTO_GET } from '../api';
-import Photos from './Photos';
-import styles from './FeedPhotos.module.css';
-import Spinner from '../Components/Spinner';
+import React, { useEffect } from "react";
+import useFetch from "../Hooks/useFetch";
+import { PHOTO_GET } from "../api";
+import Photos from "./Photos";
+import styles from "./FeedPhotos.module.css";
+import Spinner from "../Components/Spinner";
 
-const FeedPhotos = ({ setModal, page, setPage, total }) => {
+const FeedPhotos = ({
+  setModal,
+  page,
+  total,
+  infinite,
+  loadingApi,
+  setAwaitApi,
+}) => {
   const { data, request, loading } = useFetch();
-  //const [infinite, setInfinite] = useState(false);
-  const infinite = useRef(false); // ✅
-  const loadingApi = useRef(false); // guard síncrono
-  const [awaitApi, setAwaitApi] = useState(false);
+
+  const loadingApiRef = loadingApi;
+  const infiniteApiRef = infinite;
 
   useEffect(() => {
     if (!infinite.current) {
-      loadingApi.current = true; // ← falta isso aqui
+      loadingApiRef.current = true; // ← falta isso aqui
       async function requestMore() {
         const { endpoint, options } = PHOTO_GET(total, page, 0);
         const { json, response } = await request(endpoint, options);
@@ -25,40 +31,22 @@ const FeedPhotos = ({ setModal, page, setPage, total }) => {
             loadingApi.current = false;
           }, 500);
         } else {
-          infinite.current = true;
+          infiniteApiRef.current = true;
         }
       }
 
       requestMore();
     }
-  }, [request, page, infinite, total]);
-
-  useEffect(() => {
-    function carregarMais() {
-      if (awaitApi || infinite.current || loadingApi.current) return;
-
-      if (
-        window.innerHeight + window.scrollY >=
-        document.body.offsetHeight - 100
-      ) {
-        if (data && data.length > 0 && !awaitApi && !loadingApi.current) {
-          loadingApi.current = true;
-          setAwaitApi(true);
-          setPage((prev) => [...prev, prev.length + 1]);
-        } else {
-          infinite.current = true;
-        }
-      }
-    }
-
-    window.addEventListener('wheel', carregarMais);
-    window.addEventListener('scroll', carregarMais);
-
-    return () => {
-      window.removeEventListener('wheel', carregarMais);
-      window.removeEventListener('scroll', carregarMais);
-    };
-  }, [data, awaitApi, infinite, setPage]);
+  }, [
+    request,
+    page,
+    infinite,
+    total,
+    infiniteApiRef,
+    loadingApi,
+    loadingApiRef,
+    setAwaitApi,
+  ]);
 
   if (loading) return <Spinner />;
 
