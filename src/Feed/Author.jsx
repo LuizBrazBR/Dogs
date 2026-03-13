a4ba2a5bd4c35971227877efc1d833d8c32823dc
Luiz Braz <luiz.braz@evolua.software>
2026-03-13 14:30:35 -0300

    Correção de modal

--- src/Feed/Author.jsx
+++ src/Feed/Author.jsx
@@ -1,28 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { PHOTO_GET } from "../api";
-import useFetch from "../Hooks/useFetch";
+
 import Photos from "./Photos";
 import FeedPhoto from "./FeedPhoto";
+import Feed from "./Feed";
 
 const Author = () => {
   const { user } = useParams();
-  const { data, request } = useFetch();
-
-  useEffect(() => {
-    async function chamarApi() {
-      const { endpoint, options } = PHOTO_GET(9, 1, user);
-      await request(endpoint, options);
-    }
-
-    chamarApi();
-  }, [request, user]);
 
   return (
-    <section className="container">
-      <h1 className="title">{user}</h1>
-      <FeedPhoto data={data} />
-    </section>
+    <>
+      <section className="container">
+        <h1 className="title">{user}</h1>
+      </section>
+      <Feed user={user} />
+    </>
   );
 };
 
